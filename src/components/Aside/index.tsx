caf6514fbb4c71d29bc969788cd5f4f0294c09c9
Dalmo Felipe <dalmo.felipe@gmail.com>
2024-04-14 14:15:02 -0300

    implementa aside menu

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -6,8 +6,34 @@ import "./styles.css";
 function Aside() {
     return (
         <aside className="aside">
-            <GiDeathStar />
-            <p>ASIDE</p>
+
+            <div className="logo">
+                <GiDeathStar />
+                <span>
+                    <span>ADMIN </span> 
+                    <strong>BASE</strong>
+                </span>
+            </div>
+
+            <menu className="aside-menu">
+
+                <div className="amenu-top">
+                    <ul className="aside-links">
+                        <li><a className="aside-link" href="/">Content 1</a></li>
+                        <li><a className="aside-link" href="/">Content 2</a></li>
+                        <li><a className="aside-link" href="/">Content 3</a></li>
+                    </ul>
+                </div>
+
+                <div className="amenu-bottom">
+                    <ul className="aside-links">
+                        <li><a className="aside-link" href="/">Content 1</a></li>
+                        <li><a className="aside-link" href="/">Content 2</a></li>
+                    </ul>
+                </div>
+
+            </menu>
+            
         </aside>
     );
 }
